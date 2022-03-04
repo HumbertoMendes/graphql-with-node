bb922bd96c2338728da242366d4dddd87649d5c2
Humberto Mendes <humbertoarm@gmail.com>
2022-03-03 19:19:36 -0600

    * Moved code to e-commerce-app folder

---
+++ car-shop-project/index.js
@@ -0,0 +1,40 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    cars: [Car!]!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!,
+    features: [GroupFeatures!]!,
+    applyFeaturesSeparately: Boolean!,
+    cars: [Car!]!,
+    name: String!,
+    imageId: ID!,
+    bodyHtml: String!,
+  }
+
+  type GroupFeatures {
+    feature: String!,
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      cars: () => [{ id: 1, color: "blue", make: "Toyota" }],
+    },
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
